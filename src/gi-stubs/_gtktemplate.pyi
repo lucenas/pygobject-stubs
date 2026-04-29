4f43bc4f45202b487a5e34927feeec37b6782d2d
lucenas <87463503+lucenas@users.noreply.github.com>
2026-04-29 10:12:44 +1000

    Update _gtktemplate.pyi

--- src/gi-stubs/_gtktemplate.pyi
+++ src/gi-stubs/_gtktemplate.pyi
@@ -1,7 +1,7 @@
 from typing import Any
 from typing import overload
-from typing import TypeVar
 from typing import Self
+from typing import TypeVar
 
 import os
 from collections.abc import Callable
